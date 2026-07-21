b84b98fcb11df521ecaef33999efaa6848d5ad45
harishmallipeddi <167404446+harishmallipeddi@users.noreply.github.com>
2026-07-21 11:40:14 +0530

    Add files via upload

---
+++ coditional_task.py
@@ -0,0 +1,55 @@
+
+# Write a program to check if a user has entered the correct OTP (1234). If correct, display "OTP Verified".
+# user=int(input("enter the otp:"))
+# if user==1234:
+#     print("otp verified successfully")
+# else:
+#     print("incorrect otp!")
+    
+# Write a program to check if the mobile battery percentage is below 20. If yes, display "Please Charge Your Phone".
+# mobile=int(input("enter the mobile battery percentage:"))
+# if mobile<20:
+#     print("Please Charge Your Phone")
+# else:
+#     print("your battery have engough charge")
+
+# Write a program to check if a bank account balance is greater than ₹10,000. If yes, display "Eligible for Premium Account".
+# bank_balance=int(input("enter your balance :"))
+# if bank_balance>10000:
+#     print("Eligible for Premium Account")
+# else:
+#     print("not Eligible for Premium Account")
+
+# Write a program to check if a person has a valid ticket (has_ticket = True). If yes, display "Welcome to the Movie".
+# has_ticket=True
+# if has_ticket:
+#     print("welcome to the movie")
+# else:
+#     print("Sorry, you need a valid ticket")
+    
+# Write a program to check whether a person is eligible to vote.
+# age=int(input("enter your age:"))
+# if age>=18:
+#     print("eligible for vote")
+# else:
+#     print("not eligible for vote")
+
+
+# Write a program to check whether a customer is eligible for free delivery (bill ≥ ₹1000).
+# customer_bill=int(input("enter your bill:"))
+# if customer_bill>=1000:
+#     print("eligible for free delivery")
+# else:
+    # print("not eligible for free delivery")
+    
+# Write a program to check whether a user has enough balance to withdraw ₹5000.
+
+user_balance=int(input("enter your balance:"))
+if user_balance>5000:
+    print("user can withdraw")
+else:
+    print("not eligible for with draw")
+
+
+
+
